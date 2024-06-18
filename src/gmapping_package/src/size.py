c342acc3cae527f6d428b1fb748c93cdf4204450
38563541 <richby.cheng@gmail.com>
2024-06-18 17:03:49 +0800

    chore: Add new map files for navigation and update launch files for gmapping and amcl

--- src/gmapping_package/src/size.py
+++ src/gmapping_package/src/size.py
@@ -1,22 +1,13 @@
 #!/usr/bin/env python3
 import rospy
-import sys
-import rosbag
 from sensor_msgs.msg import LaserScan
 
-def callback(msg):
-    # Serialize the message to a string
-    serialized_message = rospy.msg.serialize_message(msg)
-
-    # Calculate the size of the serialized message in bytes
-    size_in_bytes = sys.getsizeof(serialized_message)
-
-    # Print the size of the message
-    rospy.loginfo('Size of the message: %s bytes', size_in_bytes)
+def callback(data):
+    rospy.loginfo("Number of range readings per scan: %d", len(data.ranges))
 
 def listener():
-    rospy.init_node('message_size_listener', anonymous=True)
-    rospy.Subscriber('/scan', LaserScan, callback)
+    rospy.init_node('scan_size_listener', anonymous=True)
+    rospy.Subscriber("downsampled_scan", LaserScan, callback)
     rospy.spin()
 
 if __name__ == '__main__':
